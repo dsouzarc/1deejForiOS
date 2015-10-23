b2447741a8253ce1389eb65a10fcac0b15145f2c
Ryan D'souza <dsouzarc@gmail.com>
2015-10-23 19:48:37 -0400

    First commit with view controller

---
+++ 1Deej/DJViewController.h
@@ -0,0 +1,13 @@
+//
+//  DJViewController.h
+//  1Deej
+//
+//  Created by Ryan D'souza on 10/23/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DJViewController : UIViewController
+
+@end
---
+++ 1Deej/DJViewController.m
@@ -0,0 +1,37 @@
+//
+//  DJViewController.m
+//  1Deej
+//
+//  Created by Ryan D'souza on 10/23/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "DJViewController.h"
+
+@interface DJViewController ()
+
+@end
+
+@implementation DJViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
